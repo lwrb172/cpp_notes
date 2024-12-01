eeb5367e0656760561bc07a78ed2875ae24227cd
lwrb172 <lwrbielak@gmail.com>
2024-12-01 23:10:30 +0100

    ll update

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -12,70 +12,84 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-    int value;
-    Node* next;
+class LinkedList {
+    struct Node {
+        int data;
+        Node* next;
+        
+        Node(int value) : data(value), next(nullptr) {}
+    };
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void removeFront() {
+        if (head == nullptr) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;    
+    }
+
+    void clear() {
+        while (head != nullptr) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+
+    int length() {
+        int result = 0;
+        Node* curr = head;
+        while (curr != nullptr) {
+            result++;
+            curr = curr->next;
+        }
+        return result;
+    }
+
+    void print() {
+        Node* tmp = head;
+        while (tmp != nullptr) {
+            cout << tmp->data << " -> ";
+            tmp = tmp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+    ~LinkedList() {
+        clear();
+    }
 };
 
-void printList(Node* n) {
-    while (n != nullptr) {
-        cout << n->value << endl;
-        n = n->next;
-    }
-}
-
-void insertAtTheFront(Node** head, int value) {
-    Node* node = new Node();
-    node->value = value;
-    node->next = *head;
-    *head = node;
-}
-
-void insertAtTheEnd(Node** head, int value) {
-    Node* node = new Node();
-    node->value = value;
-    node->next = nullptr;
-    if (*head == nullptr) {
-        *head = node;
-        return;
-    }
-    Node* last = *head;
-    while (last->next != nullptr) {
-        last = last->next;
-    }
-    last->next = node;
-}
-
-void insertAfter(Node* previous, int value) {
-    if (previous == nullptr) {
-        cout << "previous cannot be NULL";
-        return;
-    }
-    Node* node = new Node();
-    node->value = value;
-    node->next = previous->next;
-    previous->next = node;
-}
-
 int main() {
-    Node* head = new Node();
-    Node* second = new Node();
-    Node* third = new Node();
+    LinkedList list;
 
-    head->value = 1;
-    head->next = second;
-    second->value = 2;
-    second->next = third;
-    third->value = 3;
-    third->next = nullptr;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.print();
+    // output: 3 -> 2 -> 1 -> nullptr
+    list.clear();
 
-    insertAtTheFront(&head, -1);
-    insertAtTheFront(&head, -2);
-    insertAtTheEnd(&head, 4);
-    insertAtTheEnd(&head, 5);
-    insertAfter(head, -1);
-    insertAfter(second, 2);
-    printList(head);
+    for(int i = 3; i > 0; i--) {
+        list.push(i);
+    }
+    list.removeFront();
+    list.print();
+    // output: 2 -> 3 -> nullptr
+
+    cout << "length = " << list.length() << endl; // 2
 
     return 0;
 }
