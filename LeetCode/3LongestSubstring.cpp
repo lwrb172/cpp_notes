cec4a0f8a335cb08425feb99f02bf900e54fc9e4
lwrb172 <lwrbielak@gmail.com>
2025-05-20 21:29:47 +0200

    lc 3

---
+++ LeetCode/3LongestSubstring.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+// sliding window algorithm
+// unordered_set about 2x faster than set, because ordering
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        unordered_set<char> sub;
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            while (sub.find(s[right]) != sub.end()) {
+                sub.erase(s[left]);
+                left++;
+            }
+            sub.insert(s[right]);
+            maxLength = max(maxLength, right-left+1);
+        }
+        return maxLength;
+    }
+};
+
+int main() {
+    Solution* solution = new Solution;
+    string s = "abcabcbb";
+    int len = solution->lengthOfLongestSubstring(s);
+    std::cout << len << std::endl;
+
+    return 0;
+}
